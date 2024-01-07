2a9bba3b51a3226d2d24e8c7f56a73c962929096
RIZKY <RIZKY@rizky>
2024-01-07 23:30:05 +0700

    Kalkulator

---
+++ src/loginsignup/Loginsignup.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginsignup;
+
+/**
+ *
+ * @author RIZKY
+ */
+public class Loginsignup {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+         login loginFrame = new login();
+         loginFrame.setVisible(true);
+         loginFrame.pack();
+         loginFrame.setLocationRelativeTo(null);
+    }
+    
+}
